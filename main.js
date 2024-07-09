2275f0a067ff1302321e7e493c208e9d61d28b5b
Hyejin Kim <khyejin08@naver.com>
2024-07-09 21:35:46 +0900

    배포 주소 변경

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let news = [];
 
 
 const getLatestNews = async () => {
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  //const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  const url = new URL(`http://https://hyejinkim-news-times.netlify.app/top-headlines`);
   const response = await fetch(url);
   const data = await response.json();
   news = data.articles;
